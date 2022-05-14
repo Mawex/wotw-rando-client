768a0fc679535bd6b201c8c7038eca876b7dde14
Timo Schwarzer <timo@schwarzer.dev>
2022-05-14 22:53:36 +0200

    PoC ghosts

--- projects/InjectDLL/randomizer/input/rando_bindings.cpp
+++ projects/InjectDLL/randomizer/input/rando_bindings.cpp
@@ -8,6 +8,7 @@
 #include <Common/ext.h>
 
 #include <Il2CppModLoader/common.h>
+#include <Il2CppModLoader/console.h>
 #include <Il2CppModLoader/interception_macros.h>
 #include <Il2CppModLoader/il2cpp_helpers.h>
 
@@ -16,6 +17,7 @@
 #include <unordered_set>
 
 #include <magic_enum/include/magic_enum.hpp>
+#include <thread>
 
 using namespace modloader;
 
@@ -209,12 +211,125 @@ namespace randomizer
             csharp_bridge::on_action_triggered(action);
         }
 
+        STATIC_IL2CPP_BINDING(, RaceSystem, app::RaceSystem*, get_Instance, ())
+        STATIC_IL2CPP_BINDING(, GhostManager, app::GhostRecorder*, GetOrCreateRecorder, ())
+        IL2CPP_BINDING(, RaceSystem, app::GhostRecorder*, get_Recorder, (app::RaceSystem* this_ptr))
+        IL2CPP_BINDING(, RaceSystem, app::GhostService*, get_GhostService, (app::RaceSystem* this_ptr))
+        IL2CPP_BINDING(, GhostRecorder, void, InitializeRecorder, (app::GhostRecorder* this_ptr, app::String* outputPath))
+        IL2CPP_BINDING(, GhostRecorder, void, RegisterPlugin, (app::GhostRecorder* this_ptr, app::IRecorderPlugin* plugin))
+        IL2CPP_BINDING(, GhostRecorder, void, StartRecorder, (app::GhostRecorder* this_ptr))
+        IL2CPP_BINDING(, GhostRecorder, void, StopRecorder, (app::GhostRecorder* this_ptr))
+        IL2CPP_BINDING(, GhostRecorder, void, WriteToFile, (app::GhostRecorder* this_ptr, app::String* path, bool allowAppend))
+        IL2CPP_BINDING(, GhostPlayer, void, RegisterPlugin, (app::GhostPlayer* this_ptr, app::IRecorderPlugin* plugin))
+        NAMED_IL2CPP_BINDING(, GhostCharacterPlugin, void, .ctor, ctor, (app::GhostCharacterPlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostStateMachinePlugin, void, .ctor, ctor, (app::GhostStateMachinePlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostCharacterAbilitiesPlugin, void, .ctor, ctor, (app::GhostCharacterAbilitiesPlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostGenericEventsPlugin, void, .ctor, ctor, (app::GhostGenericEventsPlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostCharacterStatesPlugin, void, .ctor, ctor, (app::GhostCharacterStatesPlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostTimelineEventsPlugin, void, .ctor, ctor, (app::GhostTimelineEventsPlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostWorldObjectsLifetimePlugin, void, .ctor, ctor, (app::GhostWorldObjectsLifetimePlugin* this_ptr))
+        NAMED_IL2CPP_BINDING(, GhostRecorderData, void, .ctor, ctor, (app::GhostRecorderData* this_ptr))
+        IL2CPP_BINDING(, GhostRecorderData, void, LoadFromFile, (app::GhostRecorderData* this_ptr, app::String* filePath))
+        IL2CPP_BINDING(, GhostService, void, PopulateGhosts, (app::GhostService* this_ptr, app::GhostPlayer__Array*))
+        IL2CPP_BINDING(, GhostService, void, RunGhosts, (app::GhostService* this_ptr))
+        IL2CPP_BINDING(, GhostManager, app::GhostPlayer*, createGhost, (app::GhostManager* this_ptr, app::String* ghostInputFile))
+
+        app::GhostRecorder* recorder = nullptr;
+        constexpr std::string_view OUTPUT_PATH = "C:\\moon\\rando.ghost";
+
+        void test1(Action action, bool pressed)
+        {
+            console::console_send("race1 debug:");
+
+            // auto race_system = RaceSystem::get_Instance();
+            recorder = GhostManager::GetOrCreateRecorder();
+
+            auto* const character_plugin = il2cpp::create_object<app::GhostCharacterPlugin>("", "GhostCharacterPlugin");
+            GhostCharacterPlugin::ctor(character_plugin);
+            GhostRecorder::RegisterPlugin(recorder, reinterpret_cast<app::IRecorderPlugin*>(character_plugin));
+
+            auto* const state_machine_plugin = il2cpp::create_object<app::GhostStateMachinePlugin>("", "GhostStateMachinePlugin");
+            GhostStateMachinePlugin::ctor(state_machine_plugin);
+            GhostRecorder::RegisterPlugin(recorder, reinterpret_cast<app::IRecorderPlugin*>(state_machine_plugin));
+
+            auto* const character_abilities_plugin = il2cpp::create_object<app::GhostCharacterAbilitiesPlugin>("", "GhostCharacterAbilitiesPlugin");
+            GhostCharacterAbilitiesPlugin::ctor(character_abilities_plugin);
+            GhostRecorder::RegisterPlugin(recorder, reinterpret_cast<app::IRecorderPlugin*>(character_abilities_plugin));
+
+            auto* const generic_events_plugin = il2cpp::create_object<app::GhostGenericEventsPlugin>("", "GhostGenericEventsPlugin");
+            GhostGenericEventsPlugin::ctor(generic_events_plugin);
+            GhostRecorder::RegisterPlugin(recorder, reinterpret_cast<app::IRecorderPlugin *>(generic_events_plugin));
+
+            auto* const timeline_events_plugin = il2cpp::create_object<app::GhostTimelineEventsPlugin>("", "GhostTimelineEventsPlugin");
+            GhostTimelineEventsPlugin::ctor(timeline_events_plugin);
+            GhostRecorder::RegisterPlugin(recorder, reinterpret_cast<app::IRecorderPlugin *>(timeline_events_plugin));
+
+            auto* const world_objects_lifetime_plugin = il2cpp::create_object<app::GhostWorldObjectsLifetimePlugin>("", "GhostWorldObjectsLifetimePlugin");
+            GhostWorldObjectsLifetimePlugin::ctor(world_objects_lifetime_plugin);
+            GhostRecorder::RegisterPlugin(recorder, reinterpret_cast<app::IRecorderPlugin *>(world_objects_lifetime_plugin));
+
+            GhostRecorder::InitializeRecorder(recorder, il2cpp::string_new(OUTPUT_PATH));
+            GhostRecorder::StartRecorder(recorder);
+        }
+
+        void test2(Action action, bool pressed)
+        {
+            console::console_send("race2 debug:");
+            GhostRecorder::StopRecorder(recorder);
+            GhostRecorder::WriteToFile(recorder, il2cpp::string_new(OUTPUT_PATH), false);
+        }
+
+        app::GhostPlayer* ghost_player = nullptr;
+        void test3(Action action, bool pressed)
+        {
+            console::console_send("race3 debug:");
+
+            auto manager_class = il2cpp::get_class<app::GhostManager__Class>("", "GhostManager");
+            auto manager = manager_class->static_fields->instance;
+
+            auto* const recorder_data = il2cpp::create_object<app::GhostRecorderData>("", "GhostRecorderData");
+            GhostRecorderData::ctor(recorder_data);
+            GhostRecorderData::LoadFromFile(recorder_data, il2cpp::string_new(OUTPUT_PATH));
+
+            ghost_player = GhostManager::createGhost(manager, il2cpp::string_new(OUTPUT_PATH));
+            ghost_player->fields.GhostRecorderData = recorder_data;
+
+            auto ghost_player_vector = std::vector<app::GhostPlayer*> { ghost_player };
+            auto ghost_player_array = il2cpp::array_new<app::GhostPlayer__Array>(reinterpret_cast<Il2CppClass *>(ghost_player->klass), ghost_player_vector);
+
+            auto ghost_service = RaceSystem::get_GhostService(RaceSystem::get_Instance());
+            GhostService::PopulateGhosts(ghost_service, ghost_player_array);
+            GhostService::RunGhosts(ghost_service);
+        }
+
+        void test4(Action action, bool pressed)
+        {
+            if (ghost_player->fields.m_isStarted)
+                console::console_send("started");
+        }
+
+        IL2CPP_INTERCEPT(, GhostPlayer, void, Run, (app::GhostPlayer* this_ptr)) {
+            auto* const timeline_events_plugin = il2cpp::create_object<app::GhostTimelineEventsPlugin>("", "GhostTimelineEventsPlugin");
+            GhostTimelineEventsPlugin::ctor(timeline_events_plugin);
+            GhostPlayer::RegisterPlugin(this_ptr, reinterpret_cast<app::IRecorderPlugin *>(timeline_events_plugin));
+
+            auto* const world_objects_lifetime_plugin = il2cpp::create_object<app::GhostWorldObjectsLifetimePlugin>("", "GhostWorldObjectsLifetimePlugin");
+            GhostWorldObjectsLifetimePlugin::ctor(world_objects_lifetime_plugin);
+            GhostPlayer::RegisterPlugin(this_ptr, reinterpret_cast<app::IRecorderPlugin *>(world_objects_lifetime_plugin));
+
+            GhostPlayer::Run(this_ptr);
+        }
+
         void initialize()
         {
-            add_on_pressed_callback(Action::Binding1, csharp_callback);
-            add_on_pressed_callback(Action::Binding2, csharp_callback);
-            add_on_pressed_callback(Action::Binding3, csharp_callback);
-            add_on_pressed_callback(Action::Binding4, csharp_callback);
+            add_on_pressed_callback(Action::Binding1, test1);
+            add_on_pressed_callback(Action::Binding2, test2);
+            add_on_pressed_callback(Action::Binding3, test3);
+            add_on_pressed_callback(Action::Binding4, test4);
+            // add_on_pressed_callback(Action::Binding1, csharp_callback);
+            // add_on_pressed_callback(Action::Binding2, csharp_callback);
+            // add_on_pressed_callback(Action::Binding3, csharp_callback);
+            // add_on_pressed_callback(Action::Binding4, csharp_callback);
             add_on_pressed_callback(Action::Binding5, csharp_callback);
             add_on_pressed_callback(Action::Reload, csharp_callback);
             add_on_pressed_callback(Action::ShowLastPickup, csharp_callback);
