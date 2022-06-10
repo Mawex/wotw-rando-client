43f45256aeb2998da89f049fa798b1467ea3fd99
Timo Schwarzer <timo@schwarzer.dev>
2022-06-10 22:28:14 +0200

    Don't need concatenation operations here since these are different tokens, leads to parser warnings otherwise

--- projects/Il2CppModLoader/interception_macros.h
+++ projects/Il2CppModLoader/interception_macros.h
@@ -11,10 +11,10 @@
 #define IL2CPP_REGISTER_METHODINFO(address, name)
 
 #define IL2CPP_INTERCEPT(method_namespace, return_type, method_name, params)                                                                         \
-    static_assert(std::is_same<decltype(method_namespace##::##method_name), return_type(*) params>::value, "incorrect function type");            \
+    static_assert(std::is_same<decltype(method_namespace::method_name), return_type(*) params>::value, "incorrect function type");            \
     return_type(*method_name##_original) params = nullptr;                                                                                             \
     return_type method_name##_intercept params;                                                                                                    \
-    modloader::intercept::intercept binding_##method_name(reinterpret_cast<void**>(&method_namespace##::##method_name), reinterpret_cast<void**>(&method_name##_original), method_name##_intercept, #method_name); \
+    modloader::intercept::intercept binding_##method_name(reinterpret_cast<void**>(&method_namespace::method_name), reinterpret_cast<void**>(&method_name##_original), method_name##_intercept, #method_name); \
     return_type method_name##_intercept params
 
 #define GUARD(namezpace, nested, klass_name, name, ...)                                                                                                           \
