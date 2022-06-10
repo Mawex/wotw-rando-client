ccfc5739898814263adc690146922adaa1d1e227
Timo Schwarzer <timo@schwarzer.dev>
2022-06-11 01:06:37 +0200

    Refactor better_menus

--- projects/InjectDLL/features/better_menus.cpp
+++ projects/InjectDLL/features/better_menus.cpp
@@ -3,14 +3,10 @@
 #include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception_macros.h>
 #include <Il2CppModLoader/windows_api/console.h>
+#include <Il2CppModLoader/app/methods/FaderB.h>
+#include <Il2CppModLoader/app/methods/MenuScreenManager__PostFadeMenuOpen_d__100.h>
 
-#include <enums/game_event.h>
-#include <event_bus.h>
-#include <features/scenes/scene_load.h>
-#include <game/game.h>
-#include <uber_states/uber_state_helper.h>
-#include <utils/operations.h>
-
+using namespace app::methods;
 using namespace modloader;
 using modloader::win::console::console_flush;
 using modloader::win::console::console_send;
@@ -19,7 +15,7 @@ namespace {
     // Skip fade to black when opening menus
     bool skip_fade_to_black = false;
 
-    IL2CPP_INTERCEPT(, FaderB, void, Fade, (app::FaderB * this_ptr, float fadeInDuration, float fadeStayDuration, float fadeOutDuration, app::Action* fadeInComplete, app::Action* fadeOutComplete, bool skipCameraMoveToTarget)) {
+    IL2CPP_INTERCEPT(app::methods::FaderB, void, Fade, (app::FaderB * this_ptr, float fadeInDuration, float fadeStayDuration, float fadeOutDuration, app::Action* fadeInComplete, app::Action* fadeOutComplete, bool skipCameraMoveToTarget)) {
         if (!skip_fade_to_black)
             FaderB::Fade(this_ptr, fadeInDuration, fadeStayDuration, fadeOutDuration, fadeInComplete, fadeOutComplete, skipCameraMoveToTarget);
     }
@@ -28,10 +24,10 @@ namespace {
      * In vanilla, there's a 100ms WaitForSeconds coroutine running before opening the menu.
      * We skip that by advancing the coroutine enumerator twice.
      */
-    INTERNAL_INTERCEPT(0x9E65B0, bool, MenuScreenManager_PostFadeMenuOpen_d_100_MoveNext, (app::MenuScreenManager_PostFadeMenuOpen_d_100 * this_ptr)) {
+    IL2CPP_INTERCEPT(app::methods::MenuScreenManager__PostFadeMenuOpen_d__100, bool, MoveNext, (app::MenuScreenManager_PostFadeMenuOpen_d_100 * this_ptr)) {
         ScopedSetter setter(skip_fade_to_black, true);
-        MenuScreenManager_PostFadeMenuOpen_d_100_MoveNext(this_ptr);
-        return MenuScreenManager_PostFadeMenuOpen_d_100_MoveNext(this_ptr);
+        MenuScreenManager__PostFadeMenuOpen_d__100::MoveNext(this_ptr);
+        return MenuScreenManager__PostFadeMenuOpen_d__100::MoveNext(this_ptr);
     }
 
     /*
