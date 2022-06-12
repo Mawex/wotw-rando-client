1419c097e62508b2a4fa58f9d15fd162457a8ff9
Timo Schwarzer <timo@schwarzer.dev>
2022-06-12 21:40:34 +0200

    Refactor deflector_without_bash

--- projects/InjectDLL/game/behaviour_changes/deflector_without_bash.cpp
+++ projects/InjectDLL/game/behaviour_changes/deflector_without_bash.cpp
@@ -1,18 +1,18 @@
 #include <constants.h>
 #include <game/player.h>
 
-#include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception.h>
 #include <Il2CppModLoader/interception_macros.h>
+#include <Il2CppModLoader/app/methods/Projectile.h>
 
 namespace {
-    IL2CPP_INTERCEPT(, Projectile, bool, HandleMirrorStrike, (app::Projectile * this_ptr, app::Damage* damage)) {
+    IL2CPP_INTERCEPT(Projectile, bool, HandleMirrorStrike, (app::Projectile * this_ptr, app::Damage * damage)) {
         bool ret = false;
         if (game::player::has_ability(app::AbilityType__Enum::Bash))
-            ret = Projectile::HandleMirrorStrike(this_ptr, damage);
+            ret = next::Projectile::HandleMirrorStrike(this_ptr, damage);
         else {
             game::player::set_ability(app::AbilityType__Enum::Bash, true);
-            ret = Projectile::HandleMirrorStrike(this_ptr, damage);
+            ret = next::Projectile::HandleMirrorStrike(this_ptr, damage);
             game::player::set_ability(app::AbilityType__Enum::Bash, false);
         }
 
