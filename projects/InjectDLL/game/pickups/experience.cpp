e5ba9f6b5de0a8f321959b0b79026d6d745c8ac3
Timo Schwarzer <timo@schwarzer.dev>
2022-06-14 21:57:20 +0200

    Refactor experience (2)

--- projects/InjectDLL/game/pickups/experience.cpp
+++ projects/InjectDLL/game/pickups/experience.cpp
@@ -3,11 +3,13 @@
 #include <Il2CppModLoader/app/methods/SeinLevel.h>
 #include <game/pickups/pickups.h>
 
-IL2CPP_INTERCEPT(Game::Orbs_OrbDisplayText, app::ExpText*, Create, (app::Transform * target, app::Vector3 offset, int32_t value)) {
-    return game::pickups::should_collect_pickup() ? next::Game::Orbs_OrbDisplayText::Create(target, offset, value) : nullptr;
-}
+namespace {
+    IL2CPP_INTERCEPT(Game::Orbs_OrbDisplayText, app::ExpText*, Create, (app::Transform * target, app::Vector3 offset, int32_t value)) {
+        return game::pickups::should_collect_pickup() ? next::Game::Orbs_OrbDisplayText::Create(target, offset, value) : nullptr;
+    }
 
-IL2CPP_INTERCEPT(SeinLevel, void, set_Experience, (app::SeinLevel * this_ptr, int32_t value)) {
-    if (game::pickups::should_collect_pickup())
-        next::SeinLevel::set_Experience(this_ptr, value);
+    IL2CPP_INTERCEPT(SeinLevel, void, set_Experience, (app::SeinLevel * this_ptr, int32_t value)) {
+        if (game::pickups::should_collect_pickup())
+            next::SeinLevel::set_Experience(this_ptr, value);
+    }
 }
