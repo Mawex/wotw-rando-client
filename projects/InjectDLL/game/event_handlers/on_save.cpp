61ba1388b27999992156ea0f4105e37cf5ffdf17
Timo Schwarzer <timo@schwarzer.dev>
2022-06-16 19:48:02 +0200

    Refactor on_save (2)

--- projects/InjectDLL/game/event_handlers/on_save.cpp
+++ projects/InjectDLL/game/event_handlers/on_save.cpp
@@ -19,45 +19,45 @@ using namespace app::methods;
 extern bool temporary_glide_switch;
 
 namespace {
-    IL2CPP_INTERCEPTGameController, void, CreateCheckpoint, (app::GameController * this_ptr, bool doPerformSave, bool respectRestrictCheckpointZone)) {
+    IL2CPP_INTERCEPT(GameController, void, CreateCheckpoint, (app::GameController * this_ptr, bool doPerformSave, bool respectRestrictCheckpointZone)) {
         game::event_bus().trigger_event(GameEvent::CreateCheckpoint, EventTiming::Start);
         next::GameController::CreateCheckpoint(this_ptr, doPerformSave, respectRestrictCheckpointZone);
         game::event_bus().trigger_event(GameEvent::CreateCheckpoint, EventTiming::End);
     }
 
-    IL2CPP_INTERCEPTNewGameAction, void, Perform, (app::NewGameAction * this_ptr, app::IContext* context)) {
+    IL2CPP_INTERCEPT(NewGameAction, void, Perform, (app::NewGameAction * this_ptr, app::IContext* context)) {
         game::event_bus().trigger_event(GameEvent::NewGame, EventTiming::Start);
         next::NewGameAction::Perform(this_ptr, context);
         game::event_bus().trigger_event(GameEvent::NewGame, EventTiming::End);
     }
 
-    IL2CPP_INTERCEPTSaveGameController, void, SaveToFile, (app::SaveGameController * this_ptr, int32_t slotIndex, int32_t backupIndex, app::Byte__Array* bytes)) {
+    IL2CPP_INTERCEPT(SaveGameController, void, SaveToFile, (app::SaveGameController * this_ptr, int32_t slotIndex, int32_t backupIndex, app::Byte__Array* bytes)) {
         game::event_bus().trigger_event(GameEvent::CreateSave, EventTiming::Start);
         csharp_bridge::on_save(slotIndex, backupIndex);
         next::SaveGameController::SaveToFile(this_ptr, slotIndex, backupIndex, bytes);
         game::event_bus().trigger_event(GameEvent::CreateSave, EventTiming::End);
     }
 
-    IL2CPP_INTERCEPTSaveSlotBackupsManager, void, PerformBackup, (app::SaveSlotBackupsManager * this_ptr, app::SaveSlotBackup* saveSlot, int32_t backupIndex, app::String* backupName)) {
+    IL2CPP_INTERCEPT(SaveSlotBackupsManager, void, PerformBackup, (app::SaveSlotBackupsManager * this_ptr, app::SaveSlotBackup* saveSlot, int32_t backupIndex, app::String* backupName)) {
         game::event_bus().trigger_event(GameEvent::CreateBackup, EventTiming::Start);
         csharp_bridge::on_save(saveSlot->fields.Index, backupIndex);
         next::SaveSlotBackupsManager::PerformBackup(this_ptr, saveSlot, backupIndex, backupName);
         game::event_bus().trigger_event(GameEvent::CreateBackup, EventTiming::End);
     }
 
-    IL2CPP_INTERCEPTSaveGameController, void, OnFinishedLoading, (app::SaveGameController * this_ptr)) {
+    IL2CPP_INTERCEPT(SaveGameController, void, OnFinishedLoading, (app::SaveGameController * this_ptr)) {
         game::event_bus().trigger_event(GameEvent::FinishedLoadingSave, EventTiming::Start);
         next::SaveGameController::OnFinishedLoading(this_ptr);
         game::event_bus().trigger_event(GameEvent::FinishedLoadingSave, EventTiming::End);
     }
 
-    IL2CPP_INTERCEPTSaveGameController, void, RestoreCheckpoint, (app::SaveGameController * this_ptr)) {
+    IL2CPP_INTERCEPT(SaveGameController, void, RestoreCheckpoint, (app::SaveGameController * this_ptr)) {
         game::event_bus().trigger_event(GameEvent::FinishedLoadingCheckpoint, EventTiming::Start);
         next::SaveGameController::RestoreCheckpoint(this_ptr);
         game::event_bus().trigger_event(GameEvent::FinishedLoadingCheckpoint, EventTiming::End);
     }
 
-    IL2CPP_INTERCEPTSeinHealthController, void, OnRespawn, (app::SeinHealthController * this_ptr)) {
+    IL2CPP_INTERCEPT(SeinHealthController, void, OnRespawn, (app::SeinHealthController * this_ptr)) {
         game::event_bus().trigger_event(GameEvent::Respawn, EventTiming::Start);
         next::SeinHealthController::OnRespawn(this_ptr);
         game::event_bus().trigger_event(GameEvent::Respawn, EventTiming::End);
