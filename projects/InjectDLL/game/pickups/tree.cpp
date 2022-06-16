bd6de3569b00b211a276109bbf774bf73cbfe4ad
Timo Schwarzer <timo@schwarzer.dev>
2022-06-16 14:34:09 +0200

    Refactor tree

--- projects/InjectDLL/game/pickups/tree.cpp
+++ projects/InjectDLL/game/pickups/tree.cpp
@@ -1,10 +1,9 @@
 #include <constants.h>
-#include <randomizer/conditions/new_setup_state_override.h>
 #include <uber_states/uber_state_interface.h>
 
-#include <Il2CppModLoader/common.h>
+#include <Il2CppModLoader/app/methods/GetAbilityOnCondition.h>
+#include <Il2CppModLoader/app/methods/Moon/uberSerializationWisp/DesiredPlayerAbilityState.h>
 #include <Il2CppModLoader/interception_macros.h>
-#include <Il2CppModLoader/windows_api/console.h>
 
 #include <interop/csharp_bridge.h>
 
@@ -13,14 +12,14 @@ namespace {
         return tree_abilities.find(tree) != tree_abilities.end();
     }
 
-    IL2CPP_INTERCEPT(Moon.uberSerializationWisp, DesiredPlayerAbilityState, bool, IsFulfilled, (app::DesiredPlayerAbilityState * this_ptr)) {
+    IL2CPP_INTERCEPT(Moon::uberSerializationWisp::DesiredPlayerAbilityState, bool, IsFulfilled, (app::DesiredPlayerAbilityState * this_ptr)) {
         if (is_tree(this_ptr->fields.Ability))
             return csharp_bridge::is_tree_activated(this_ptr->fields.Ability);
         else
-            return DesiredPlayerAbilityState::IsFulfilled(this_ptr);
+            return next::Moon::uberSerializationWisp::DesiredPlayerAbilityState::IsFulfilled(this_ptr);
     }
 
-    IL2CPP_INTERCEPT(, GetAbilityOnCondition, void, AssignAbility, (app::GetAbilityOnCondition * this_ptr)) {
+    IL2CPP_INTERCEPT(GetAbilityOnCondition, void, AssignAbility, (app::GetAbilityOnCondition * this_ptr)) {
         auto ability = this_ptr->fields.Ability->fields.Ability;
         if (is_tree(ability)) {
             uber_states::UberState(UberStateGroup::Tree, static_cast<int>(ability)).set(1);
