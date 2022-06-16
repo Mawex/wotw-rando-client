c942433bb286161415515970d6bfc51698d7a85b
Timo Schwarzer <timo@schwarzer.dev>
2022-06-16 19:15:44 +0200

    Refactor death_listener

--- projects/InjectDLL/features/trackers/death_listener.cpp
+++ projects/InjectDLL/features/trackers/death_listener.cpp
@@ -1,21 +1,18 @@
-#include <features/trackers/death_listener.h>
-
-#include <Il2CppModLoader/il2cpp_helpers.h>
-#include <Il2CppModLoader/interception_macros.h>
-#include <Il2CppModLoader/windows_api/console.h>
 #include <Il2CppModLoader/app/methods/Moon/EnemyEntity.h>
 #include <Il2CppModLoader/app/methods/SeinDamageReciever.h>
+#include <Il2CppModLoader/il2cpp_helpers.h>
+#include <Il2CppModLoader/interception_macros.h>
 
 #include <interop/csharp_bridge.h>
 
 namespace {
-    IL2CPP_INTERCEPT(Moon::EnemyEntity, void, OnDied, (app::EnemyEntity * this_ptr, app::DamageResult* result)) {
+    IL2CPP_INTERCEPT(Moon::EnemyEntity, void, OnDied, (app::EnemyEntity * this_ptr, app::DamageResult result)) {
         // app::Type* type = this_ptr->fields.m_enemyType; actual class.
         auto* go = il2cpp::unity::get_game_object(this_ptr);
         const auto name = il2cpp::unity::get_object_name(go);
         next::Moon::EnemyEntity::OnDied(this_ptr, result);
         if (csharp_bridge::on_enemy_death != nullptr)
-            csharp_bridge::on_enemy_death(name.c_str(), result->Damage->fields.m_damageType);
+            csharp_bridge::on_enemy_death(name.c_str(), result.Damage->fields.m_damageType);
     }
 
     IL2CPP_INTERCEPT(SeinDamageReciever, void, OnKill, (app::SeinDamageReciever * this_ptr, app::Damage* damage)) {
@@ -26,4 +23,4 @@ namespace {
         if (csharp_bridge::on_player_death != nullptr)
             csharp_bridge::on_player_death(name.c_str(), damage->fields.m_damageType);
     }
-}
+} // namespace
