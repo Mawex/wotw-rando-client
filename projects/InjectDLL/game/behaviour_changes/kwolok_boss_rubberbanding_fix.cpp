0e2f3b3f9d1e68dcf8783f9bddb6f8629b0ac516
Timo Schwarzer <timo@schwarzer.dev>
2022-06-24 01:32:09 +0200

    Add back Kwolok rubberbanding

--- projects/InjectDLL/game/behaviour_changes/kwolok_boss_rubberbanding_fix.cpp
+++ projects/InjectDLL/game/behaviour_changes/kwolok_boss_rubberbanding_fix.cpp
@@ -1,39 +1,15 @@
 #include <Common/ext.h>
-#include <Il2CppModLoader/app/methods/KwolokBossEscapeBehaviour.h>
-#include <Il2CppModLoader/app/methods/Moon/Timeline/TimelineEntity.h>
-#include <Il2CppModLoader/app/methods/Moon/ScalableAnimationPlayer.h>
-#include <Il2CppModLoader/app/methods/Moon/AnimationPlayer.h>
+#include <Il2CppModLoader/app/methods/Moon/ScalableAnimationPlayer_ScalingInterval.h>
 #include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception_macros.h>
-#include <Il2CppModLoader/windows_api/console.h>
 #include <event_bus.h>
 #include <features/scenes/scene_load.h>
-#include <game/game.h>
-#include <utils/misc.h>
 
 using namespace modloader;
 using namespace app::methods;
 using namespace utils;
 
 namespace {
-    ObjectReference<app::ScalableAnimationPlayer> scalable_animation_player_stink_s;
-
-    IL2CPP_INTERCEPT(KwolokBossEscapeBehaviour, void, SetSpeedToAnimations, (app::KwolokBossEscapeBehaviour * this_ptr, float speed)) {
-        // modloader::win::console::console_send(format("Updated speed to %f", speed));
-        // modloader::win::console::console_flush();
-
-        next::KwolokBossEscapeBehaviour::SetSpeedToAnimations(this_ptr, speed);
-
-        if (scalable_animation_player_stink_s.is_valid()) {
-            // Moon::ScalableAnimationPlayer::SetSpeed(scalable_animation_player_stink_s.ptr, 0.f);
-            Moon::AnimationPlayer::SetTimeScale(reinterpret_cast<app::AnimationPlayer*>(scalable_animation_player_stink_s.ptr), 0.1f);
-        }
-    }
-
-    IL2CPP_INTERCEPT(Moon::ScalableAnimationPlayer, void, SetSpeed, (app::ScalableAnimationPlayer* this_ptr, float speed)) {
-        next::Moon::ScalableAnimationPlayer::SetSpeed(this_ptr, 40.f);
-    }
-
     void on_scene_load(scenes::SceneLoadEventMetadata* metadata, EventTiming timing) {
         if (metadata->state != app::SceneState__Enum::Loaded) {
             return;
@@ -53,7 +29,15 @@ namespace {
             );
 
             if (il2cpp::unity::is_valid(timeline_go)) {
-                scalable_animation_player_stink_s.set_reference(il2cpp::unity::get_component<app::ScalableAnimationPlayer>(timeline_go, "Moon", "ScalableAnimationPlayer"));
+                auto scalable_animation_player = il2cpp::unity::get_component<app::ScalableAnimationPlayer>(timeline_go, "Moon", "ScalableAnimationPlayer");
+
+                // Moon why
+                auto interval = il2cpp::create_object<app::ScalableAnimationPlayer_ScalingInterval>("Moon", "ScalableAnimationPlayer", "ScalingInterval");
+                Moon::ScalableAnimationPlayer_ScalingInterval::ctor(interval);
+                interval->fields.NormalizedStart = 0.f;
+                interval->fields.NormalizedDuration = 1.f;
+
+                il2cpp::invoke(scalable_animation_player->fields.ScalingAllowedIntervals, "Add", interval);
             }
         }
     }
